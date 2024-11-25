a2f1381f97227ab0cc87bd6012020dec844a33df
i-shintaro <shintaroiwashita7857@gmail.com>
2024-11-25 16:26:58 +0900

    問題 10 配列を利用したrubyプログラムの作成

--- drill_10.rb
+++ drill_10.rb
@@ -11,7 +11,7 @@ def search(target_num, input)
       end
     end
     puts "その数は含まれていません"
-  end
+end
   #①inputといった変数に配列の数字を代入する
   input = [3, 5, 9 ,12, 15, 21, 29, 35, 42, 51, 62, 78, 81, 87, 92, 93]
   #②searchメソッドを呼び出し実引数(11, input)をもたせる

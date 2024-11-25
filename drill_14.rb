9c23d577ddbd74ce4a3304ec94c6468b957eaa1a
i-shintaro <shintaroiwashita7857@gmail.com>
2024-11-25 16:27:43 +0900

    問題 14 if,else問題

--- drill_14.rb
+++ drill_14.rb
@@ -1,13 +1,16 @@
+#② メソッドin1to10の仮引数numとoutside_modeに、それぞれ11とfalseが渡されます。
 def in1to10(num, outside_mode)
-  # numが1以上10以下の間にあるか、あるいは 'outside_mode' がtrueならば
-  if (num >= 1 && num <= 10) || outside_mode
-    puts "True"
-  else
-    puts "False"
-  end
+  #③ 条件式では、numが1以上10以下であるか、またはoutside_modeがtrueであれば"True"を出力します。
+  #   しかし、ここではnumは11で範囲外であり、かつoutside_modeがfalseです。
+  #   したがって、条件は成立せず"False"が出力されます。
+    if (num >= 1 && num <= 10) || outside_mode
+      puts "True"
+    else
+      puts "False"
+    end
 end
-
-# 呼び出し例
-in1to10(5,false)
-in1to10(11,false)
-in1to10(11,true)
+  
+  #① in1to10メソッドを呼び出し、11とfalseをそれぞれ第一引数と第二引数として渡します。
+  #   numには11が渡され、outside_modeにはfalseが渡されます。
+  in1to10(11,true)
+  

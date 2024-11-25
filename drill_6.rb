76d11755450b2330f704dbf95c99d7750f859823
i-shintaro <shintaroiwashita7857@gmail.com>
2024-11-25 16:27:25 +0900

    問題 6 引数の基礎

--- drill_6.rb
+++ drill_6.rb
@@ -1,5 +1,10 @@
+# ①get_weather_forecaメソッドを定義する
 def get_weather_forecast(weather)
+# ④ メソッド内でputsを使い、"明日の天気は..."という文字列を出力する
+#    このとき、式展開で引数のweatherの値が使用される
   puts "明日の天気は#{weather}です"
 end
 
+# ②getget_weather_forecastメソッドを呼び出す
+# ③実引数として"晴れ"を渡す
 get_weather_forecast("晴れ")
